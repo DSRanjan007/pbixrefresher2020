b937495947153a624e0f8900f875995e50a003dd
DSRanjan007 <84807441+DSRanjan007@users.noreply.github.com>
2023-02-20 12:13:27 +0530

    Add files via upload

--- pbixrefresher.py
+++ pbixrefresher.py
@@ -7,6 +7,11 @@ import pyautogui
 from pywinauto.application import Application
 from pywinauto import timings
 
+def type_keys(string, element):
+    """Type a string char by char to Element window"""
+    for char in string:
+        element.type_keys(char)
+
 def main():   
 	# Parse arguments from cmd
 	parser = argparse.ArgumentParser()
@@ -14,7 +19,7 @@ def main():
 	parser.add_argument("--workspace", help = "name of online Power BI service work space to publish in", default = "My workspace")
 	parser.add_argument("--refresh-timeout", help = "refresh timeout", default = 30000, type = int)
 	parser.add_argument("--no-publish", dest='publish', help="don't publish, just save", default = True, action = 'store_false' )
-	parser.add_argument("--init-wait", help = "initial wait time on startup", default = 60, type = int)
+	parser.add_argument("--init-wait", help = "initial wait time on startup", default = 15, type = int)
 	args = parser.parse_args()
 
 	timings.after_clickinput_wait = 1
@@ -36,20 +41,31 @@ def main():
 	os.system('start "" "' + WORKBOOK + '"')
 	print("Waiting ",INIT_WAIT,"sec")
 	time.sleep(INIT_WAIT)
-
-	# Connect pywinauto
+    
 	print("Identifying Power BI window")
 	app = Application(backend = 'uia').connect(path = PROCNAME)
 	win = app.window(title_re = '.*Power BI Desktop')
 	time.sleep(5)
+	win.wait("enabled", timeout = 30)
+	win.Save.wait("enabled", timeout = 30)
 	win.set_focus()
+	win.Home.click_input()
+	win.Save.wait("enabled", timeout = 30)
+	win.wait("enabled", timeout = 30)
 	
 	# Refresh
 	print("Refreshing")
 	win.Refresh.click_input()
-	win.wait("enabled", timeout = 300)
+	#wait_win_ready(win)
 	time.sleep(5)
 	print("Waiting for refresh end (timeout in ", REFRESH_TIMEOUT,"sec)")
+	win.wait("enabled", timeout = REFRESH_TIMEOUT)
+
+	# Save
+	print("Saving")
+	type_keys("%1", win)
+	#wait_win_ready(win)
+	time.sleep(5)
 	win.wait("enabled", timeout = REFRESH_TIMEOUT)
 
 	# Publish
@@ -67,7 +83,10 @@ def main():
 		time.sleep(10)
 		replace_dialog = win.child_window(auto_id = "KoPublishWithImpactViewDialog")
 		replace_dialog.Replace.click_input()
-		time.sleep(300)
+		time.sleep(30)
+		win["Got it"].wait('visible', timeout = REFRESH_TIMEOUT)
+		win["Got it"].click_input()
+        
 		
 	#Close
 	print("Exiting")
